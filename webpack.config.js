2f6270259047326c8eaf5138a651ee5c19ca4f15
Carlos Eduardo <eduardosantoos573@gmail.com>
2023-03-22 23:00:34 -0300

    add: files responsible for creating a bundle for production

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+module.exports = {
+	mode: 'production',
+	entry: './src/app.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: '/node_modules/',
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/env'],
+						compact: true,
+					}
+				}
+			}
+		]
+	}
+}
+// how connect mysql on prisma?
