26c6b660d1aae8cbba6af2709b6f362c74f89906
Carlos Eduardo <eduardosantoos573@gmail.com>
2023-12-16 21:21:59 -0300

    change: modify method import

--- src/routes.js
+++ src/routes.js
@@ -1,26 +1,49 @@
 import { Router } from 'express'
+
+import multer from 'multer'
+import multerConfig from './config/multer.js'
+
 import authMiddleware from'./middleware/authMiddleware.js'
 import adminMiddleware from'./middleware/adminMiddleware.js'
 import changeLogController from'./controllers/ChangeLogController.js'
+import labelController from './controllers/LabelController.js'
+import categoriesController from './controllers/CategoriesController.js'
 import userController from'./controllers/UserController.js'
 import authController from'./controllers/AuthController.js'
-import profileController from'./controllers/ProfileController.js'
 import passwordController from'./controllers/PasswordController.js'
 
 const router = Router()
 
 router.get('/changelog/:id', authMiddleware, changeLogController.index)
 router.get('/changelog', authMiddleware, changeLogController.show)
-router.post('/changelog', authMiddleware, adminMiddleware, changeLogController.execute)
+router.post('/changelog', 
+	authMiddleware, 
+	adminMiddleware, 
+	multer(multerConfig).single('file'), 
+	changeLogController.execute)
 router.put('/changelog/:id', authMiddleware, adminMiddleware, changeLogController.update)
-router.delete('/changelog/:id', authMiddleware, adminMiddleware, changeLogController.destroy)
+router.delete('/changelog/:id/image/:imageId', authMiddleware, adminMiddleware, changeLogController.destroy)
 
-router.get('/profile', authMiddleware, profileController.show)
-router.put('/profile', authMiddleware, profileController.updated)
+router.get('/label/list', authMiddleware, labelController.listLabel)
+router.post('/label', authMiddleware, adminMiddleware, labelController.createLabel)
+router.patch('/label/:labelId', authMiddleware, adminMiddleware, labelController.editLabel)
+
+router.get('/categories/category/list', 
+	authMiddleware, 
+	categoriesController.listCotegories)
+router.post('/categories', authMiddleware, adminMiddleware, categoriesController.createCategories)
+router.patch('/categories/:categorieId/category/:categoryId', 
+	authMiddleware, 
+	adminMiddleware, 
+	categoriesController.editCategories)
 
 router.put('/forgetpassword', passwordController.changePassword)
 
+router.get('/user/list', authMiddleware, adminMiddleware, userController.userList)
+router.get('/user', authMiddleware, userController.show)
 router.post('/user', userController.store)
+router.patch('/user', authMiddleware, userController.updated)
+router.delete('/user/:userId', authMiddleware, adminMiddleware, userController.deleteUser)
 
 router.post('/auth/login', authController.authenticate)
 
