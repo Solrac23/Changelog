7de26e88d177d1c866674a3ec216c76d2bc5751e
Carlos Eduardo <eduardosantoos573@gmail.com>
2023-12-16 21:34:40 -0300

    change: new validations

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,15 +3,64 @@ import { crypt } from './util/cryptography.js'
 import { AppErros } from '../errors/appErros.js'
 
 export default {
+	async userList(req, res){
+		try {
+			const users = await prismaClient.user.findMany({
+				select: {
+					id: true,
+					name: true,
+					email: true,
+					role: true,
+					company: {
+						select: {
+							id_company: true,
+							CompanyName: true,
+							uf: true,
+							city: true,
+						}
+					}
+				},
+			})
+
+			return res.status(200).json(users)
+		} catch (err) {
+			console.warn(err.message)
+		}
+	},
+
+	async show(req, res){
+		const {userId} = req
+
+		if(!userId) {
+			throw new AppErros('Profile not exist or Id not found.')
+		}
+
+		const userProfile = await prismaClient.user.findFirst({
+			where:{
+				id: userId,
+			},
+			select: {
+				id: true,
+				name: true,
+				email: true,
+				createAt: true,
+				updateAt: true,
+				company: true
+			}
+		})
+
+		return res.status(200).json(userProfile)
+	},
+
 	async store(req, res){
 		const {
 			name,
 			email,
 			password,
 			role,
-			nameCompany,
+			CompanyName,
 			uf,
-			city,
+			city
 		} = req.body
 	
 		const pass = crypt(password)
@@ -30,25 +79,101 @@ export default {
 			throw new AppErros('User already exist!', 409)
 		}
 
-		const users = await prismaClient.user.create({
-			data: {
-				name,
-				email,
-				password: pass,
-				role,
-				company:{
-					create:{
-						nameCompany,
-						uf,
-						city,
-					},
+		try {
+			const user = await prismaClient.user.create({
+				data: {
+					name,
+					email,
+					password: pass,
+					role,
+					company: {
+						create:{
+							CompanyName,
+							uf,
+							city,
+						},						
+					}
 				},
-			},
-			include: {
-				company: true,
-			},
+				include: {
+					company: true,
+				},
+			})
+			return res.status(201).json(user)
+		} catch (err) {
+			console.error(err.message)
+		}	
+	
+	},
+	async updated(req, res){
+		const {
+			name, 
+			email,
+			CompanyName,
+			uf,
+			city
+		} = req.body
+
+		const { userId } = req
+
+		if(!userId) {
+			throw new AppErros('Profile not exist or Id not found.')
+		}
+
+		try {
+			const updateUser = await prismaClient.user.update({
+				where: {
+					id: userId
+				},
+				data: {
+					name,
+					email, 
+					company: {
+						update: {
+							CompanyName,
+							uf,
+							city
+						}
+					}
+				},
+				select: {
+					id: true,
+					name: true,
+					email: true,
+					company: true,
+				}
+			})
+	
+			return res.status(201).json(updateUser)
+		} catch (err) {
+			console.error(err.message)
+		}
+	},
+
+	async deleteUser(req, res){
+		const {userId} = req.params
+
+		const userAlreadyExist = await prismaClient.user.findFirst({
+			where: {
+				id: userId,
+			}
 		})
-		return res.status(201).json(users)
-	}
+
+		if(!userAlreadyExist){
+			throw new AppErros('Id not found or already deleted!', 404)
+		}
+
+		try {
+			await prismaClient.user.delete({
+				where: {
+					id: userId,
+				},
+			})
+	
+			return res.sendStatus(204)
+		} catch (err) {
+			console.error(err.message)
+		}
+		
+	},
 	
 }
