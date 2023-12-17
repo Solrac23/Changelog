c3f34d055a20fd1b0ab113941f8083c0a89b6db7
Carlos Eduardo <eduardosantoos573@gmail.com>
2023-12-16 21:21:26 -0300

    change: modify method import and link files

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 import express from 'express'
-// const cookieSession = require('cookie-session')
+import path, {dirname} from 'node:path'
+import { fileURLToPath } from 'node:url'
 import 'express-async-errors'
 import router from './routes.js'
 import {connection} from './database/connection.js'
 import cors from 'cors'
 import morgan from 'morgan'
-import { errors } from 'celebrate'
+import { AppErros } from './errors/appErros.js'
 
 const app = express()
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 if(process.env.NODE_ENV === 'development'){
 	app.use(morgan('dev'))
@@ -27,36 +30,18 @@ app.use(express.json())
 
 app.set('trust proxy', 1)
 app.disable('x-powered-by')
-// let expiryDate = new Date(Date.now() + process.env.COOKIE_EXPIRES_IN * 60 * 60 * 1000)
-// app.use(cookieSession({
-// 	name:'authcookie',
-// 	keys:[process.env.SECRET_KEY],
-// 	secure: false,
-// 	httpOnly: true,
-// 	domain: 'localhost',
-// 	path: '/auth/login',
-// 	expires: expiryDate,
-// 	sameSite: 'strict',
-// }))
 app.use(router)
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')))
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, _next) => {
-	if(err) {
+	if(err instanceof AppErros) {
 		return res.status(err.statusCode).json({ message: err.message})
 	}
 	
 	return res.status(500).json({ 
 		status: 'Error',
-		message: `Internal server error${err.message }`
+		message: `Internal server error ${err.message }`
 	})
 })
-// eslint-disable-next-line no-unused-vars
-app.use(errors((err, req, res, _next) => {
-	if (err) {
-		return res.status(400).json({ message: err.message })
-	}
-	console.log(err.stack)
-	return res.status(500).json({ message: err.message })
-}))
 
 export {app}
